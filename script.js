4cde5ea9584756df30da43b63357407d34bd4813
FelipeGabrill <felipegabrielodss@gmail.com>
2024-08-06 04:10:59 -0300

    Adicionando filtro de categorias

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ async function buscarEMostarVideos() {
                     <div class="descricao-video"> 
                         <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
                         <h3 class="titulo-video">${video.titulo}</h3>
-                        <p class="titulo-canal">${video.descricao}</p>  
+                        <p class="titulo-canal">${video.descricao}</p> 
+                        <p class="categoria" hidden>${video.categoria}</p> 
                     </div>
                 </li>
                 `;
@@ -47,5 +48,26 @@ function filtrarPesquisa() {
         }
     } else {
         video.style.display = "block";
+    }
+}
+
+const botaoCategoria = document.querySelectorAll(".superior__item");
+
+botaoCategoria.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute("name");
+    botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria));
+});
+
+function filtrarPorCategoria(filtro) {
+    const videos = document.querySelectorAll(".videos__item");
+    for (let video of videos) {
+        let categoria = video.querySelector(".categoria").textContent.toLowerCase();
+        let valorFiltro = filtro.toLowerCase();
+
+        if(!categoria.includes(valorFiltro) && valorFiltro != 'tudo') {
+            video.style.display = "none";
+        } else {
+            video.style.display = "block";
+        }
     }
 }
